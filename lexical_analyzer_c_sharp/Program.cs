3d13a4fc6967cae0be90a582711b0f1266feb44d
madbastardd <bastardd666@gmail.com>
2016-02-18 22:54:57 +0200

    start

---
+++ lexical_analyzer_c_sharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lexical_analyzer_c_sharp {
+    class Program {
+        enum LEXEM_TYPE {
+            SPACE = 1,
+            ONE_SYMBOL = 2,
+            MULTY_SYMBOL = 4,
+            KEYWORDS = 8,
+            CONST = 16,
+            IDENTIFIERS = 32
+        };
+        static void Main(string[] args) {
+            System.Collections.Generic.List<LEXEM_TYPE> attributes = new System.Collections.Generic.List<LEXEM_TYPE>();
+            for (byte ind = 0; ind < 128; ind++) {
+                if ((char)ind >= 'A' && (char)ind <= 'Z')
+                    attributes.Add(LEXEM_TYPE.IDENTIFIERS | LEXEM_TYPE.KEYWORDS);
+                else
+                    attributes.Add(0);
+            }
+
+            Table table = new Table();
+            table.insert("dimasik");
+            //table.insert("dimasik");
+            table.insert("dimasik1");
+            table.exists("dimasik");
+        }
+    }
+}
---
+++ lexical_analyzer_c_sharp/Table.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lexical_analyzer_c_sharp {
+    class Table : IEnumerable {
+        ushort current_index;
+        SortedDictionary<ushort, string> data;
+
+        public Table(ushort _current_index = 0) {
+            this.current_index = _current_index;
+            this.data = new SortedDictionary<ushort, string>();
+        }
+
+        ~Table() {
+            this.data.Clear();
+        }
+
+        public string this[ushort index] {
+            get {
+                var result = this.data[index];
+                return result;
+            }
+            private set {
+                this.data.Add(index, value);
+            }
+        }
+
+        void insert(KeyValuePair<ushort, string> pair) {
+            try {
+                this.exists(pair.Value);
+                throw new ArgumentException();
+            } catch (Exception ex) when (ex is ArgumentNullException || ex is InvalidOperationException) {
+                this[pair.Key] = pair.Value;
+            }
+            
+        }
+
+        public void insert(string _value) {
+            try {
+                this.insert(new KeyValuePair<ushort, string>(this.current_index++, _value));
+            } catch (Exception ex) when (ex is ArgumentException || ex is ArgumentNullException) {
+                this.current_index--;
+                throw ex;
+            }    
+        }
+
+        public ushort exists(string _value) {
+            var result = from res in this.data
+                         where res.Value == _value
+                         select res;
+
+            var key = result.First().Key;
+            return key;
+        }
+
+        public IEnumerator GetEnumerator() {
+            return this.data.GetEnumerator();
+        }
+    }
+}
