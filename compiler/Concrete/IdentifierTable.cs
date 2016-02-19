65cfb4e543ff1038d628199643f4684a37757849
madbastardd <bastardd666@gmail.com>
2016-02-19 17:23:28 +0200

    created attribute class

---
+++ compiler/Concrete/IdentifierTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concrete.TableSpace;
+
+namespace lexical_analyzer_c_sharp.Concrete {
+    class IdentifierTable : Table {
+        IdentifierTable(ushort _index = 0) : base(_index) {
+            //creates empty table
+        }
+    }
+}
--- compiler/Program.cs
+++ compiler/Program.cs
@@ -3,30 +3,9 @@ using Concrete.TableSpace;
 
 namespace lexical_analyzer_c_sharp {
     class Program {
-        static readonly UInt16
-            ERROR = 0,
-            SPACE = 1,
-            ONE_SYMBOL = 2,
-            MULTY_SYMBOL = 4,
-            KEYWORDS = 8,
-            CONST = 16,
-            IDENTIFIERS = 32;
+        
         
         static void Main(string[] args) {
-            System.Collections.Generic.List<UInt16> attributes = new System.Collections.Generic.List<UInt16>();
-            for (byte ind = 0; ind < 128; ind++) {
-                if ((char)ind >= 'A' && (char)ind <= 'Z' || (char)ind >= 'a' && (char)ind <= 'z') {
-                    //keyword or identifier
-                    attributes.Add((UInt16)(IDENTIFIERS | KEYWORDS));
-                }
-                else if ((char)ind >= '0' && (char)ind <= '9') {
-                    //constant
-                    attributes.Add((UInt16)(CONST));
-                }
-                else {
-                    attributes.Add((UInt16)(ERROR));
-                }
-            }
 
             Table table = new Table();
 
